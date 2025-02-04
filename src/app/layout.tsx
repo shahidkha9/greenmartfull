1bc383b6210b8881c01b29405eab81b573d2b302
MUHAMMAD SHAHID <154737925+shahidkha9@users.noreply.github.com>
2025-02-04 18:39:49 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,10 @@ import Footer from '@/app/components/Footer'
 import Banner from '@/app/components/Banner'
 import { CartProvider } from '@/app/contexts/CartContext'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata: Metadata = { <meta name="google-site-verification" content="e81gHaHwsPOQr5MiIrm1Jryn35M5mAJjp2apcTpGnxY" />
   title: 'GreenMart - Your Eco-Friendly Store',
   description: 'Shop sustainably with GreenMart, your one-stop eco-friendly e-commerce store',
 }
